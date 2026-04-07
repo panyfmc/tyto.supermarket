cb7167fbbb8f70d8bc4c8fb4bfaef9cb7a0a0bd9
panyfmc <paulavanessa010@gmail.com>
2026-04-07 18:38:08 -0300

    refactor: remove funcao adicionar item à venda

--- apps/sales/models.py
+++ apps/sales/models.py
@@ -2,31 +2,23 @@ from django.db import models
 from apps.products.models import Product
 from apps.clients.models import Client
 
-class SaleItem(models.Model):
-    sale = models.ForeignKey('Sale', on_delete=models.CASCADE, related_name='items')
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.IntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def save(self, *args, **kwargs):
-        if self.product.stock < self.quantity:
-            raise ValueError("Estoque insuficiente")
-
-        self.product.stock -= self.quantity
-        self.product.save()
-
-        super().save(*args, **kwargs)
-
+#vender um item a um cliente cadastrado
 class Sale(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
-    total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    item = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    total = models.DecimalField(max_digits=10, decimal_places=2, editable=False)
+
+    def save(self, *args, **kwargs):
+        if self.item.stock < self.quantity:
+            raise ValueError("Estoque insuficiente")
+        self.item.stock -= self.quantity
+        self.item.save()
+
+        self.total = self.item.price * self.quantity
+        super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"Venda {self.id}"
-
-    def calculate_total(self):
-        total = sum(item.price * item.quantity for item in self.items.all())
-        self.total = total
-        self.save()
+        return f"{self.item.name} - {self.quantity} unidades"
         
