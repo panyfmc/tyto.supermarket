9863ec754ab389726ae98e360b1e23b57ad329de
panyfmc <paulavanessa010@gmail.com>
2026-04-07 18:36:17 -0300

    feat: funcao que lista as vendas

--- apps/sales/views.py
+++ apps/sales/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Sale
 
-# Create your views here.
+def sale_list(request):
+    sales = Sale.objects.all()
+    return render(request, 'sales.html', {'sales': sales})
